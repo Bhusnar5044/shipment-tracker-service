ee5798d272a9bf8b97c5f235a73312bb697cbc11
Pradip Bhusnar <bhusnarpradip@gmail.com>
2024-08-18 00:10:10 +0530

    PB | added authorize middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,19 +1,19 @@
 import { NextFunction, Response } from 'express';
 import { verify } from 'jsonwebtoken';
-import { SECRET_KEY } from '@config';
+import { JWT_SECRET } from '@config';
 import { HttpException } from '@exceptions/HttpException';
 import { DataStoredInToken, RequestWithUser } from '@interfaces/auth.interface';
-import userModel from '@models/users.model';
+import { User } from '@models/user.model';
 
-const authMiddleware = async (req: RequestWithUser, res: Response, next: NextFunction) => {
+export const authMiddleware = async (req: RequestWithUser, res: Response, next: NextFunction) => {
   try {
     const Authorization = req.cookies['Authorization'] || (req.header('Authorization') ? req.header('Authorization').split('Bearer ')[1] : null);
 
     if (Authorization) {
-      const secretKey: string = SECRET_KEY;
+      const secretKey: string = JWT_SECRET;
       const verificationResponse = (await verify(Authorization, secretKey)) as DataStoredInToken;
       const userId = verificationResponse._id;
-      const findUser = await userModel.findById(userId);
+      const findUser = await User.findById(userId);
 
       if (findUser) {
         req.user = findUser;
@@ -29,4 +29,11 @@ const authMiddleware = async (req: RequestWithUser, res: Response, next: NextFun
   }
 };
 
-export default authMiddleware;
+export const authorize = (roles: string[]) => {
+  return (req: RequestWithUser, res: Response, next: NextFunction) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({ message: 'Access denied' });
+    }
+    next();
+  };
+};
