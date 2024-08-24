895f7859cfebacadeb50cabd46ffa0780240b2f6
Pradip Bhusnar <bhusnarpradip@gmail.com>
2024-08-24 23:02:58 +0530

    PB | updated base route

--- src/app.ts
+++ src/app.ts
@@ -76,7 +76,7 @@ class App {
 
   private initializeRoutes(routes: Routes[]) {
     routes.forEach(route => {
-      this.app.use('/', route.router);
+      this.app.use('/api/v1/', route.router);
     });
   }
 
