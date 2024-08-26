f446462b923ee5bfaf04e629faa74dd4d232cf47
Pradip Bhusnar <bhusnarpradip@gmail.com>
2024-08-26 18:12:50 +0530

    PB | added route

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,10 @@ import IndexRoute from '@routes/index.route';
 import UsersRoute from '@routes/users.route';
 import ShipmentRoute from '@routes/shipment.route';
 import validateEnv from '@utils/validateEnv';
+import CustomerRoute from './routes/customer.route';
 
 validateEnv();
 
-const app = new App([new IndexRoute(), new UsersRoute(), new AuthRoute(), new ShipmentRoute()]);
+const app = new App([new IndexRoute(), new UsersRoute(), new CustomerRoute(), new AuthRoute(), new ShipmentRoute()]);
 
 app.listen();
